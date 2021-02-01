57ff2e2c4f29cbfc81f39c77f27b5af648cc8327
clefzz <tinker19981@hotmail.com>
2021-02-01 20:23:35 +0800

    fix: minor fix 2

--- superopt/src/main/java/sjtu/ipads/wtune/superopt/rules/validation/MalformedSubqueryFilter.java
+++ superopt/src/main/java/sjtu/ipads/wtune/superopt/rules/validation/MalformedSubqueryFilter.java
@@ -8,7 +8,7 @@ public class MalformedSubqueryFilter extends BaseMatchingRule {
   @Override
   public boolean enterSubqueryFilter(SubqueryFilter op) {
     final Operator in = op.predecessors()[1];
-    if (in.type().isFilter()) {
+    if (!in.type().isValidOutput()) {
       matched = true;
       return false;
     }
