60651a32fb37f55e6f71f6cd0fec6dbedcacedbd
Cleveland Alto <tinker19981@hotmail.com>
2021-08-23 13:54:30 +0800

    minor

--- runEnum.sh
+++ runEnum.sh
@@ -5,4 +5,4 @@ ag --nonumber '^\d+,\d+' wtune_data/enum.log >> wtune_data/completed || true
 
 cp wtune_data/enum.out "wtune_data/enum.out.$t" || true
 
-nohup gradle :superopt:run --args='runner.EnumSubstitution -parallel=true -echo -completed=wtune_data/completed 3 1' >wtune_data/enum.log 2>&1 </dev/null &
+nohup gradle :superopt:run --args="runner.EnumSubstitution -parallel=true -echo -completed=wtune_data/completed $1 $2" >wtune_data/enum.log 2>&1 </dev/null &
