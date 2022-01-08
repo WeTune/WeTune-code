8455878098d79b9b46baa9f4d6c9d411676bceba
Cleveland Alto <tinker19981@hotmail.com>
2022-01-08 13:40:32 +0800

    [feature] update script

--- scripts/enumrule.sh
+++ scripts/enumrule.sh
@@ -1,20 +1,37 @@
 #! /bin/bash
+
 cd wtune_data
-
 files=$(ls -t -1 | ag 'rule.+')
-checkpoint=
 
-while IFS= read -r line; do
-  if [ -f "${line}/checkpoint" ]; then
-    checkpoint="${line}/checkpoint"
-    break
-  fi
-done <<<"${files}"
+postional_args=()
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --rerun)
+            rerun=1
+            shift
+            ;;
+        *)
+            postional_args+=("${1}")
+            shift
+            ;;
+    esac
+done
+
+set -- "${postional_args[@]}"
+
+if [[ -z "$rerun" ]]; then
+    while IFS= read -r line; do
+      if [ -f "${line}/checkpoint" ]; then
+        checkpoint="${line}/checkpoint"
+        break
+      fi
+    done <<<"${files}"
+fi
 
 cd ..
 
 if [ -z "${checkpoint}" ]; then
-  gradle :superopt:run --args="runner.EnumRule -parallelism=32 ${1} ${2}"
+  gradle :superopt:run --args="runner.EnumRule -parallelism=32 -timeout=600000 ${1} ${2}"
 else
-  gradle :superopt:run --args="runner.EnumRule -parallelism=32 -checkpoint=${checkpoint} ${1} ${2}"
+  gradle :superopt:run --args="runner.EnumRule -parallelism=32 -timeout=600000 -checkpoint=${checkpoint} ${1} ${2}"
 fi
