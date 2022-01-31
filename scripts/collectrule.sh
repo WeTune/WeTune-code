b80c39b59a76506c8b2f905a4695d6e959b48a40
Cleveland Alto <tinker19981@hotmail.com>
2022-01-31 10:01:44 +0800

    [feature] update scripts

--- scripts/collectrule.sh
+++ scripts/collectrule.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -eu -o pipefail
-
 data_dir="${WETUNE_DATA_DIR:-wtune_data}"
 enum_dir='enumeration'
 rule_dir='rules'
@@ -18,6 +16,11 @@ while [[ $# -gt 0 ]]; do
     ;;
   esac
 done
+
+if [ -z "${partitions}" ]; then
+    echo "Please specify partition to collect. Format: <from_partition>-<to_partition>. e.g., 0-3"
+    exit 1
+fi
 
 # parse partition
 IFS='-' read -ra TMP <<<"${partitions}"
@@ -37,14 +40,16 @@ fi
 cd "${data_dir}"
 mkdir -p "${rule_dir}"
 
-t=$(date '+%m_%d_%H_%M')
+t=$(date '+%m%d%H%M%S')
 rules_file="${rule_dir}/rules.${t}.txt"
 
 for ((i = from_partition; i <= to_partition; i++)); do
-  run_dir=$(ls -t -1 | ag "run.+_${i}" | head -1)
+  run_dir=$(ls -t -1 "${enum_dir}" | ag "run.+_${i}" | head -1)
   if [ -z "${run_dir}" ] || ! [ -f "${enum_dir}/${run_dir}/success.txt" ]; then
     echo "Excepted ${data_dir}/${enum_dir}/run*_${i}/success.txt does not exist."
     exit 1
+  else
+    echo "Found partitioin file: ${data_dir}/${enum_dir}/${run_dir}/success.txt"
   fi
   cat "${enum_dir}/${run_dir}/success.txt" >>"${rules_file}"
 done
--- scripts/collectruleall.sh
+++ scripts/collectruleall.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -eu -o pipefail
-
 data_dir="${WETUNE_DATA_DIR:-wtune_data}"
 rule_dir='rules'
 
@@ -9,6 +7,7 @@ username='zhouz'
 prefix='10.0.0.10'
 default_ips=(2 3 4 5)
 wetune_path='projects/wtune-code'
+partition='0-16'
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
@@ -24,6 +23,10 @@ while [[ $# -gt 0 ]]; do
     wetune_path="$2"
     shift 2
     ;;
+  -partition)
+    partition="$2"
+    shift 2
+    ;;
   *)
     ips+=("$1")
     shift
@@ -31,6 +34,21 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 ips=(${ips[@]:-${default_ips[@]}})
+
+if [ -z "${partition}" ]; then
+    echo "Please specify partition to collect. Format: <from_partition>-<to_partition>. e.g., 0-16"
+    exit 1
+fi
+
+# parse partition
+IFS='-' read -ra TMP <<<"${partition}"
+from_partition="${TMP[0]:-0}"
+to_partition="${TMP[1]:-${from_partition}}"
+
+if [ "${from_partition}" -gt "${to_partition}" ]; then
+  echo "Wrong partition spec: ${from_partition}-${to_partition}"
+  exit 1
+fi
 
 mkdir -p "${data_dir}/${rule_dir}"
 cd "${data_dir}/${rule_dir}"
@@ -40,9 +58,17 @@ if [ -f "${rule_file}" ]; then
   truncate -s0 "${rule_file}"
 fi
 
+num_partitions="${#ips[@]}"
+((step=(to_partition-from_partition)/num_partitions))
+i="${from_partition}"
+
 for var in "${ips[@]}"; do
   ip="${prefix}${var}"
-  ssh "${username}@${ip}" "cd ${wetune_path}; scripts/collectrule.sh"
+  x="${i}"
+  ((y=x+step))
+  echo "${ip}: ${x}-${y}"
+  ssh "${username}@${ip}" "cd ${wetune_path}; scripts/collectrule.sh -partition ${x}-${y}"
   scp "${username}@${ip}:${wetune_path}/${data_dir}/${rule_dir}/rules.local.txt" "rules.${ip}.txt"
   cat "rules.${ip}.txt" >>"${rule_file}"
+  ((i=y+1))
 done
